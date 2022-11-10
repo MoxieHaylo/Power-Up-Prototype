7f0f62ca530f26b692f03ddb5d3bb8e1d255fe0e
Hayley <hayley@moxiehaylo.games>
2022-11-10 23:07:00 +1100

    Created title screen
    Added game controller and scene management

---
+++ Power Up Prototype Files/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("GameController");
+        if (objects.Length > 1)
+        {
+            Destroy(this.gameObject);
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+}
