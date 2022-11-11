85800c934d3e7009e2e7ae6eb268aacc51e3f168
Hayley <hayley@moxiehaylo.games>
2022-11-11 14:24:04 +1100

    Explosion is bigger and cleans up after itself
    What a tidy little explosion!

--- Power Up Prototype Files/Assets/Scripts/ThingGoBang.cs
+++ Power Up Prototype Files/Assets/Scripts/ThingGoBang.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ThingGoBang : MonoBehaviour
 {
+    int iterationCount = 20;
+
     private void Awake()
     {
         StartCoroutine("GoBangNow");
@@ -13,19 +15,34 @@ public class ThingGoBang : MonoBehaviour
     {
         while(true)
         {
-            GameObject cubeBits = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Rigidbody cubeRb = cubeBits.AddComponent<Rigidbody>();
-            //MeshRenderer cubeMAT = cubeBits.AddComponent<MeshRenderer>();
-            //cubeMAT.material.color = Color.red;
-            cubeRb.mass = 5;
-            //cube.transform.localPosition = new Vector3(0f, 1f, 0f);
-            //cube.transform.parent = transform;
-            //transform = new Vector3(0f, 1f, 0f);
-            cubeBits.transform.position = transform.position + new Vector3(0f, 1f, 0f);
-            cubeBits.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
-            cubeBits.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 100, 0));
-            print("'bout to go bang");
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(0.5f);
+
+            for(int i = 0; i < iterationCount; i++)
+            {
+
+                    GameObject cubeBits = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    Rigidbody cubeRb = cubeBits.AddComponent<Rigidbody>();
+                    //MeshRenderer cubeMAT = cubeBits.AddComponent<MeshRenderer>();
+                    var cubeRenderer = cubeBits.GetComponent<Renderer>();
+                    cubeRenderer.material.SetColor("_Color", Color.red);
+                    //color = Color.red;
+                    cubeRb.mass = 5;
+                    //cube.transform.localPosition = new Vector3(0f, 1f, 0f);
+                    //cube.transform.parent = transform;
+                    //transform = new Vector3(0f, 1f, 0f);
+                    cubeBits.transform.position = transform.position + new Vector3(0f, 0f, 0f);
+                    cubeBits.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+                    cubeBits.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 100, 0));
+            cubeBits.AddComponent<Shrapnel>();
+                    print("'bout to go bang");
+
+
+                    
+
+
+            }
+
+            yield return new WaitForSeconds(0.5f);
             Destroy(this.gameObject);
             yield break;
         }
