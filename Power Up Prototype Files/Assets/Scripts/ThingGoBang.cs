624a15ed6ed6346e7a8c8a4c862aebbb39f204f4
Hayley <hayley@moxiehaylo.games>
2022-11-11 12:09:11 +1100

    Explosion effects
    Explosion object now creates a cube (will multiply) on itself to imply shrapnel

--- Power Up Prototype Files/Assets/Scripts/ThingGoBang.cs
+++ Power Up Prototype Files/Assets/Scripts/ThingGoBang.cs
@@ -13,6 +13,17 @@ public class ThingGoBang : MonoBehaviour
     {
         while(true)
         {
+            GameObject cubeBits = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Rigidbody cubeRb = cubeBits.AddComponent<Rigidbody>();
+            //MeshRenderer cubeMAT = cubeBits.AddComponent<MeshRenderer>();
+            //cubeMAT.material.color = Color.red;
+            cubeRb.mass = 5;
+            //cube.transform.localPosition = new Vector3(0f, 1f, 0f);
+            //cube.transform.parent = transform;
+            //transform = new Vector3(0f, 1f, 0f);
+            cubeBits.transform.position = transform.position + new Vector3(0f, 1f, 0f);
+            cubeBits.transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+            cubeBits.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 100, 0));
             print("'bout to go bang");
             yield return new WaitForSeconds(2f);
             Destroy(this.gameObject);
